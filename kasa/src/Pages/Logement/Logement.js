c6af4fec8052f3cae287eb73b3495c41793c84a7
Julbusiness <julien.dutruch@yahoo.fr>
2023-03-07 13:03:53 +0100

    données dynamique ok sur la page logement, quelques modifs en cours pour alléger le code

--- kasa/src/Pages/Logement/Logement.js
+++ kasa/src/Pages/Logement/Logement.js
@@ -2,94 +2,84 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./Logement.css";
-import imageLogement from "../../Assets/photo-logement-desktop.png";
 import Accord from "../../components/AccordLogement/AccordLogement";
-import Carousel, {CarouselItem} from "../../components/Carousel/Carousel";
+import Carousel, { CarouselItem } from "../../components/Carousel/Carousel";
 import Rating from "../../components/Rating/Rating";
 
 export default function Logement() {
-	const [logements, setLogements] = useState([]);
 	const currentId = useParams();
+	const [data, setData] = useState([]);
 
 	useEffect(() => {
-		fetch("../../Utils/Data/data.json")
+		fetch("../../Data/data.json")
 			.then((response) => {
 				// console.log(response);
 				return response.json();
 			})
-			.then((logements) => {
+			.then((data) => {
 				// console.log(data);
-				return setLogements(logements);
+				setData(data);
 			});
 	}, []);
 
-	/* ---------------------------------- test ---------------------------------- */
-
 	console.log(currentId);
-	console.log(logements);
+	console.log(data);
 
-	//! Rendre dynamique mes elements (voir trello)
-
-	const element = logements.filter((logement) => logement.id === currentId.id);
-
-	console.log(element);
-
-	const elementAPI = [] 
+	const element = data.filter((dataElement) => dataElement.id === currentId.id);
+	let elementAPI;
 
 	if (element) {
 		console.log(element[0]);
-		elementAPI.push(element[0])
+		elementAPI = element[0];
 	}
 	
 	console.log(elementAPI);
 
-	//!
+	
+
+	//! il faut mapper sur les categories numerotation images
+
 	return (
+		elementAPI && 
 		<div className="container">
 			<div className="bloc-carousel">
 				<Carousel>
-					<CarouselItem>
-						<img src={imageLogement} alt="" />
-					</CarouselItem>
-					<CarouselItem>
-						<img src={imageLogement} alt="" />
-					</CarouselItem>
-					<CarouselItem>
-						<img src={imageLogement} alt="" />
-					</CarouselItem>
+					{elementAPI.pictures.map(picture => <CarouselItem key={picture}><img src={picture} alt="" className="picture"/></CarouselItem>)}
 				</Carousel>
 			</div>
 			<div className="bloc-profil">
 				<div className="bloc-text">
-					{/* <h1>{elementAPI[0].title}</h1> */}
-					{/* <p>{elementAPI[0].location}</p> */}
+					<h1>{elementAPI.title}</h1>
+					<p>{elementAPI.location}</p>
 				</div>
 				<div className="profil">
-					{/* <p>{elementAPI[0].host.name}</p> */}
+					<p>{elementAPI.host.name}</p>
 					<div className="avatar">
-						{/* <img src={elementAPI[0].host.picture} alt="" /> */}
+						<img src={elementAPI.host.picture} alt="" />
 					</div>
 				</div>
 			</div>
 			<div className="bloc-tag">
 				<div className="tags">
-					<div className="tag">Cozy</div>
-					<div className="tag">Canal</div>
-					<div className="tag">Paris 10</div>
+					{elementAPI.tags.map(tag => <div 
+					key={tag}
+					className="tag">{tag}</div>)}
 				</div>
 				<div className="rating">
-					<Rating />
+					<Rating 
+						rating={parseInt(elementAPI.rating)}
+					/>
 				</div>
 			</div>
 			<div className="bloc-accordeon">
 				<Accord
 					title="Description"
-					// content={elementAPI[0].description}
+					content={elementAPI.description}
 					className="accord-logement"
 				/>
-				<Accord 
+				<Accord
 					title="Equipements"
-					content="Texte dynamique à venir"
+					content={elementAPI.equipments.map(equipment => <li key={equipment} className="equipment-list">{equipment}</li>)}
 					className="accord-logement"
 				/>
 			</div>
